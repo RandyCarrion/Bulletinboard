6e0ea9b14a68b791c8b5a400fc36ee919b95544b
Randy <carrion.randy@gmail.com>
2017-11-27 15:38:05 +0100

    small update

--- index.js
+++ index.js
@@ -1,24 +1,25 @@
+/*--------------------MANDATORY in order to read the file and send info-----------*/
 const express = require("express")
-require('dotenv').load();
-const app = express()
-const pg = require('pg')
-const bodyParser = require("body-parser")
-app.use(bodyParser.urlencoded())
-const Client = pg.Client
+require('dotenv').load(); //file that has keywords and usernames "environment variables"
+const app = express() //accesing methods in the server side 
+const pg = require('pg')  //include the node postgres library
+const bodyParser = require("body-parser") //THis allows the info to be read and retreaved (parsing) the data. This must be used
+app.use(bodyParser.urlencoded()) 
+const Client = pg.Client //this is needed to use postgres 
 const client = new Client({ //this locates the server on the computer
     user: process.env.user,
     host: 'localhost',
     database: 'bulletinboard',
     port: '5432',
     password: process.env.password
-})
+}) //then this client is the command connection to the server.
 
 client.connect()//this connects to the sesrver
 
 
 app.set('view engine', 'pug')//tells that the file its reading its in pug form
 
-
+/*-------Renders INDEX page--------------*/
 app.get("/", (req, res) => { //get the homepage
     // const readquery = {
     //     text: `select * from messages;` //command into shell, what to execute
@@ -28,7 +29,7 @@ app.get("/", (req, res) => { //get the homepage
          res.render('index') //songs:songs gets the input songs into the index page
     // })
 })
-
+/*--------------gets the list from Shell and logs it--------*/
 app.get("/Bulletin", (req, res) => {
     const readquery = {
         text: `select * from messages;`
@@ -40,17 +41,16 @@ app.get("/Bulletin", (req, res) => {
     })
 })
 
-
+/*---------------Sends the inputted info to the Shell-------------*/
 app.post("/add", (req, res) => {
-    //this connects to the SQL shell tabless
-    console.log(req.body.artist, req.body.title)
+    //this connects to the SQL shell tables
+    //console.log(req.body.artist, req.body.title)
     const insertquery = {
         text: `INSERT INTO messages (title, body) values ('${req.body.artist}','${req.body.title}')`
     }
-    client.query(insertquery, function(err, response) {
+    client.query(insertquery, function(err, response) /*this response is written differently because its not the same as the res, but it is just a parameter */ {
         console.log('error', err)
     })
-    let songs = [1, 2, 3, 4, 5];
     const readquery = {
         text: `select * from messages;`
     }
@@ -62,7 +62,6 @@ app.post("/add", (req, res) => {
     })
     //songs:songs gets the input songs into the index page
 })
-
 // app.get("/form", (req, res) => {
 // 	res.redner
 // })
@@ -72,9 +71,10 @@ app.post("/add", (req, res) => {
 
 
 // 	console.log(err)
-// 	client.end() //this ends connections with the server
-// })
+	//client.end() //this ends connections with the server
+// }) If you end the connection, in order to connect again, you need to make a new client request
 
+/*--------connect to your localhost---------*/
 app.listen(3004, function() {
     console.log("yuurr")
 })
